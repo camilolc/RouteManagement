5f43e7eebc65cc9364cd67f9c44d52ea23b68a69
Camilo <kmilo3025@hotmail.com>
2023-02-23 21:45:50 -0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { Navigation } from "./routes/Navigation";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
